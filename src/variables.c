a84fab1989ff326e348ce84cf53328833f9a473d
Carlos Abalde <carlos.abalde@gmail.com>
2018-06-19 14:29:56 +0200

    Preparations to add cfg.rules() object

---
+++ src/variables.c
@@ -0,0 +1,198 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "vcl.h"
+#include "vrt.h"
+#include "cache/cache.h"
+
+#include "helpers.h"
+#include "variables.h"
+
+/******************************************************************************
+ * BASICS.
+ *****************************************************************************/
+
+static int
+variablecmp(const variable_t *v1, const variable_t *v2)
+{
+    return strcmp(v1->name, v2->name);
+}
+
+VRB_GENERATE(variables, variable, tree, variablecmp);
+
+variable_t *
+new_variable(const char *name, size_t len, const char *value)
+{
+    variable_t *result;
+    ALLOC_OBJ(result, VARIABLE_MAGIC);
+    AN(result);
+
+    result->name = strndup(name, len);
+    AN(result->name);
+
+    result->value = strdup(value);
+    AN(result->value);
+
+    return result;
+}
+
+void
+free_variable(variable_t *variable)
+{
+    free((void *) variable->name);
+    variable->name = NULL;
+
+    free((void *) variable->value);
+    variable->value = NULL;
+
+    FREE_OBJ(variable);
+}
+
+variable_t *
+find_variable(variables_t *variables, const char *name)
+{
+    variable_t variable;
+    variable.name = name;
+    return VRB_FIND(variables, variables, &variable);
+}
+
+void
+flush_variables(variables_t *variables)
+{
+    variable_t *variable, *tmp;
+    VRB_FOREACH_SAFE(variable, variables, variables, tmp) {
+        CHECK_OBJ_NOTNULL(variable, VARIABLE_MAGIC);
+        VRB_REMOVE(variables, variables, variable);
+        free_variable(variable);
+    }
+}
+
+/******************************************************************************
+ * HELPERS.
+ *****************************************************************************/
+
+unsigned
+is_set_variable(VRT_CTX, variables_t *variables, const char *name)
+{
+    AN(ctx->ws);
+    if (name != NULL) {
+        return find_variable(variables, name) != NULL;
+    }
+    return 0;
+}
+
+const char *
+get_variable(VRT_CTX, variables_t *variables, const char *name, const char *fallback)
+{
+    AN(ctx->ws);
+    const char *result = fallback;
+
+    if (name != NULL) {
+        variable_t *variable = find_variable(variables, name);
+        if (variable != NULL) {
+            CHECK_OBJ_NOTNULL(variable, VARIABLE_MAGIC);
+            result = variable->value;
+        }
+    }
+
+    if (result != NULL) {
+        result = WS_Copy(ctx->ws, result, -1);
+        AN(result);
+    }
+
+    return result;
+}
+
+static const char *json_hex_chars = "0123456789abcdef";
+
+#define DUMP_CHAR(value) \
+    do { \
+        *end = value; \
+        assert(free_ws > 0); \
+        end++; \
+        free_ws--; \
+    } while (0)
+
+#define DUMP_STRING(value) \
+    do { \
+        DUMP_CHAR('"'); \
+        for (int i = 0; value[i]; i++) { \
+            if (value[i] > 31 && value[i] != '\"' && value[i] != '\\') { \
+                DUMP_CHAR(value[i]); \
+            } else { \
+                DUMP_CHAR('\\'); \
+                switch (value[i]) { \
+                    case '\\': \
+                        DUMP_CHAR('\\'); \
+                        break; \
+                    \
+                    case '"': \
+                        DUMP_CHAR('"'); \
+                        break; \
+                    \
+                    case '\b': \
+                        DUMP_CHAR('b'); \
+                        break; \
+                    \
+                    case '\f': \
+                        DUMP_CHAR('f'); \
+                        break; \
+                    \
+                    case '\n': \
+                        DUMP_CHAR('n'); \
+                        break; \
+                    \
+                    case '\r': \
+                        DUMP_CHAR('r'); \
+                        break; \
+                    \
+                    case '\t': \
+                        DUMP_CHAR('t'); \
+                        break; \
+                    \
+                    default: \
+                        DUMP_CHAR('u'); \
+                        DUMP_CHAR('0'); \
+                        DUMP_CHAR('0'); \
+                        DUMP_CHAR(json_hex_chars[(value[i] >> 4) & 0xf]); \
+                        DUMP_CHAR(json_hex_chars[value[i] & 0xf]); \
+                        break; \
+                } \
+            } \
+        } \
+        DUMP_CHAR('"'); \
+    } while (0)
+
+const char *
+dump_variables(VRT_CTX, variables_t *variables)
+{
+    AN(ctx->ws);
+    char *result, *end;
+    variable_t *variable;
+    unsigned free_ws = WS_Reserve(ctx->ws, 0);
+    assert(free_ws > 0);
+    result = end = WS_Front(ctx->ws);
+
+    DUMP_CHAR('{');
+    VRB_FOREACH(variable, variables, variables) {
+        CHECK_OBJ_NOTNULL(variable, VARIABLE_MAGIC);
+        DUMP_STRING(variable->name);
+        DUMP_CHAR(':');
+        DUMP_STRING(variable->value);
+        DUMP_CHAR(',');
+    }
+    if (*(end - 1) == ',') {
+        *(end - 1) = '}';
+    } else {
+        DUMP_CHAR('}');
+    }
+    *end = '\0';
+
+    WS_Release(ctx->ws, end - result + 1);
+
+    return result;
+}
+
+#undef DUMP_CHAR
+#undef DUMP_STRING
---
+++ src/variables.h
@@ -0,0 +1,29 @@
+#ifndef CFG_VARIABLES_H_INCLUDED
+#define CFG_VARIABLES_H_INCLUDED
+
+#include "vtree.h"
+
+typedef struct variable {
+    unsigned magic;
+    #define VARIABLE_MAGIC 0xcb181fe6
+
+    const char *name;
+    char *value;
+
+    VRB_ENTRY(variable) tree;
+} variable_t;
+
+typedef VRB_HEAD(variables, variable) variables_t;
+
+VRB_PROTOTYPE(variables, variable, tree, variablecmp);
+
+variable_t *new_variable(const char *name, size_t len, const char *value);
+void free_variable(variable_t *variable);
+variable_t *find_variable(variables_t *variables, const char *name);
+void flush_variables(variables_t *variables);
+
+unsigned is_set_variable(VRT_CTX, variables_t *variables, const char *name);
+const char *get_variable(VRT_CTX, variables_t *variables, const char *name, const char *fallback);
+const char *dump_variables(VRT_CTX, variables_t *variables);
+
+#endif
