25ea232fb24dbd3af428ea4a72cff2aff9bf36ba
Carlos Abalde <carlos.abalde@gmail.com>
2018-06-19 13:23:12 +0200

    Preparations to add cfg.rules() object

---
+++ src/vmod_cfg_env.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "vcl.h"
+#include "vrt.h"
+#include "cache/cache.h"
+#include "vcc_cfg_if.h"
+
+#include "helpers.h"
+#include "variables.h"
+
+extern char **environ;
+
+struct vmod_cfg_env {
+    unsigned magic;
+    #define VMOD_CFG_ENV_MAGIC 0x44baed10
+    variables_t variables;
+};
+
+static void
+load_env(VRT_CTX, struct vmod_cfg_env *env)
+{
+    flush_variables(&env->variables);
+    for (int i = 0; environ[i]; i++) {
+        char *ptr = strchr(environ[i], '=');
+        if (ptr != NULL) {
+            variable_t *variable = new_variable(environ[i], ptr - environ[i], ptr + 1);
+            AZ(VRB_INSERT(variables, &env->variables, variable));
+        }
+    }
+}
+
+VCL_VOID
+vmod_env__init(VRT_CTX, struct vmod_cfg_env **env, const char *vcl_name)
+{
+    CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+    AN(env);
+    AZ(*env);
+
+    struct vmod_cfg_env *instance;
+    ALLOC_OBJ(instance, VMOD_CFG_ENV_MAGIC);
+    AN(instance);
+
+    VRB_INIT(&instance->variables);
+
+    load_env(ctx, instance);
+
+    *env = instance;
+}
+
+VCL_VOID
+vmod_env__fini(struct vmod_cfg_env **env)
+{
+    AN(env);
+    AN(*env);
+
+    struct vmod_cfg_env *instance = *env;
+    CHECK_OBJ_NOTNULL(instance, VMOD_CFG_ENV_MAGIC);
+
+    flush_variables(&instance->variables);
+
+    FREE_OBJ(instance);
+
+    *env = NULL;
+}
+
+VCL_BOOL
+vmod_env_is_set(VRT_CTX, struct vmod_cfg_env *env, VCL_STRING name)
+{
+    return is_set_variable(ctx, &env->variables, name);
+}
+
+VCL_STRING
+vmod_env_get(VRT_CTX, struct vmod_cfg_env *env, VCL_STRING name, VCL_STRING fallback)
+{
+    return get_variable(ctx, &env->variables, name, fallback);
+}
+
+VCL_STRING
+vmod_env_dump(VRT_CTX, struct vmod_cfg_env *env)
+{
+    return dump_variables(ctx, &env->variables);
+}
---
+++ src/vmod_cfg_file.c
@@ -0,0 +1,431 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <math.h>
+#include <curl/curl.h>
+
+#include "vcl.h"
+#include "vrt.h"
+#include "cache/cache.h"
+#include "vcc_cfg_if.h"
+
+#include "cJSON.h"
+#include "ini.h"
+
+#include "helpers.h"
+#include "remote.h"
+#include "variables.h"
+
+struct vmod_cfg_file {
+    unsigned magic;
+    #define VMOD_CFG_FILE_MAGIC 0x9774a43f
+
+    remote_t *remote;
+    const char *name_delimiter;
+    const char *value_delimiter;
+    variables_t *(*parse)(VRT_CTX, struct vmod_cfg_file *, const char *);
+
+    struct {
+        pthread_rwlock_t rwlock;
+        variables_t *variables;
+    } state;
+};
+
+struct file_parse_ctx {
+    struct vmod_cfg_file *file;
+    variables_t *variables;
+};
+
+/******************************************************************************
+ * INI PARSER.
+ *****************************************************************************/
+
+struct file_ini_stream_ctx {
+    const char *ptr;
+    int left;
+};
+
+static char *
+file_ini_stream_reader(char *str, int size, void *stream)
+{
+    struct file_ini_stream_ctx* ctx = (struct file_ini_stream_ctx*) stream;
+
+    if (ctx->left <= 0) {
+        return NULL;
+    }
+
+    int idx = 0;
+    char newline = '\0';
+
+    for (idx = 0; idx < size - 1; ++idx) {
+        if (idx == ctx->left) {
+            break;
+        }
+
+        if (ctx->ptr[idx] == '\n') {
+            newline = '\n';
+            break;
+        } else if (ctx->ptr[idx] == '\r') {
+            newline = '\r';
+            break;
+        }
+    }
+    memcpy(str, ctx->ptr, idx);
+    str[idx] = '\0';
+
+    ctx->ptr += idx + 1;
+    ctx->left -= idx + 1;
+    if (newline && (ctx->left > 0) && (ctx->ptr[0] == newline)) {
+        ctx->left--;
+        ctx->ptr++;
+    }
+
+    return str;
+}
+
+static int
+file_parse_ini_handler(void *c, const char *section, const char *name, const char *value)
+{
+    struct file_parse_ctx *ctx = (struct file_parse_ctx *) c;
+
+    char *buffer;
+    unsigned flatten = (section != NULL) && (strlen(section) > 0);
+    assert(asprintf(
+        &buffer,
+        "%s%s%s",
+            flatten ? section : "",
+            flatten ? ctx->file->name_delimiter : "",
+            name) > 0);
+
+    variable_t *variable = find_variable(ctx->variables, buffer);
+    if (variable == NULL) {
+        variable = new_variable(buffer, strlen(buffer), value);
+        AZ(VRB_INSERT(variables, ctx->variables, variable));
+    } else {
+        variable->value = realloc(
+            variable->value,
+            strlen(variable->value) + strlen(ctx->file->value_delimiter) + strlen(value) + 1);
+        AN(variable->value);
+        if (strlen(variable->value) > 0) {
+            strcat(variable->value, ctx->file->value_delimiter);
+        }
+        strcat(variable->value, value);
+    }
+
+    free((void *) buffer);
+
+    return 1;
+}
+
+static variables_t *
+file_parse_ini(VRT_CTX, struct vmod_cfg_file *file, const char *contents)
+{
+    variables_t *result = NULL;
+
+    struct file_ini_stream_ctx file_ini_stream_ctx = {
+        .ptr = contents,
+        .left = strlen(contents)
+    };
+
+    struct file_parse_ctx file_parse_ctx = {
+        .file = file,
+        .variables = malloc(sizeof(variables_t))
+    };
+    AN(file_parse_ctx.variables);
+    VRB_INIT(file_parse_ctx.variables);
+
+    int rc = ini_parse_stream(
+        (ini_reader) file_ini_stream_reader, &file_ini_stream_ctx,
+        file_parse_ini_handler, &file_parse_ctx);
+
+    if (rc == 0) {
+        result = file_parse_ctx.variables;
+
+        LOG(ctx, LOG_INFO,
+            "Remote successfully parsed (location=%s, format=ini)",
+            file->remote->location.raw);
+    } else {
+        flush_variables(file_parse_ctx.variables);
+        free((void *) file_parse_ctx.variables);
+
+        LOG(ctx, LOG_ERR,
+            "Failed to parse remote (location=%s, format=ini, error=%d)",
+            file->remote->location.raw, rc);
+    }
+
+    return result;
+}
+
+/******************************************************************************
+ * JSON PARSER.
+ *****************************************************************************/
+
+static void
+file_parse_json_emit(struct file_parse_ctx *ctx, const char *name, cJSON *item)
+{
+    char *value = NULL;
+    double intpart;
+
+    switch (item->type) {
+        case cJSON_False:
+            value = "false";
+            break;
+
+        case cJSON_True:
+            value = "true";
+            break;
+
+        case cJSON_Number:
+            if (modf(item->valuedouble, &intpart) == 0) {
+                assert(asprintf(&value, "%d", item->valueint) > 0);
+            } else {
+                assert(asprintf(&value, "%.3f", item->valuedouble) > 0);
+            }
+            break;
+
+        case cJSON_Raw:
+        case cJSON_String:
+            value = item->valuestring;
+            break;
+    }
+
+    if (value != NULL) {
+        variable_t *variable = new_variable(name, strlen(name), value);
+        AZ(VRB_INSERT(variables, ctx->variables, variable));
+        if (item->type == cJSON_Number) {
+            free((void *) value);
+        }
+    }
+}
+
+static void
+file_parse_json_walk(
+    struct file_parse_ctx *ctx, cJSON *item, const char *prefix, unsigned delimit)
+{
+    char *buffer;
+
+    while (item) {
+        if (item->type != cJSON_Array) {
+            assert(asprintf(
+                &buffer, "%s%s%s",
+                prefix,
+                (delimit && item->string != NULL) ? ctx->file->name_delimiter: "",
+                (item->string != NULL) ? item->string : "") >= 0);
+
+            if (item->type != cJSON_Object) {
+                file_parse_json_emit(ctx, buffer, item);
+            } else if (item->child) {
+                file_parse_json_walk(ctx, item->child, buffer, item->string != NULL);
+            }
+
+            free((void *) buffer);
+        }
+
+        item = item->next;
+    }
+}
+
+static variables_t *
+file_parse_json(VRT_CTX, struct vmod_cfg_file *file, const char *contents)
+{
+    variables_t *result = NULL;
+
+    struct file_parse_ctx file_parse_ctx = {
+        .file = file,
+        .variables = malloc(sizeof(variables_t))
+    };
+    AN(file_parse_ctx.variables);
+    VRB_INIT(file_parse_ctx.variables);
+
+    const char *error;
+    cJSON *root = cJSON_ParseWithOpts(contents, &error, 0);
+
+    if (root != NULL) {
+        if (root->type == cJSON_Object) {
+            file_parse_json_walk(&file_parse_ctx, root, "", 0);
+
+            result = file_parse_ctx.variables;
+
+            LOG(ctx, LOG_INFO,
+                "Remote successfully parsed (location=%s, format=json)",
+                file->remote->location.raw);
+        } else {
+            free((void *) file_parse_ctx.variables);
+
+            LOG(ctx, LOG_ERR,
+                "Unexpected JSON type (location=%s, format=json, type=%d)",
+                file->remote->location.raw, root->type);
+        }
+
+        cJSON_Delete(root);
+    } else {
+        free((void *) file_parse_ctx.variables);
+
+        LOG(ctx, LOG_ERR,
+            "Failed to parse remote (location=%s, format=json)",
+            file->remote->location.raw);
+    }
+
+    return result;
+}
+
+/******************************************************************************
+ * BASICS.
+ *****************************************************************************/
+
+static unsigned
+file_check_callback(VRT_CTX, void *ptr, const char *contents)
+{
+    unsigned result = 0;
+
+    struct vmod_cfg_file *file;
+    CAST_OBJ_NOTNULL(file, ptr, VMOD_CFG_FILE_MAGIC);
+
+    variables_t *variables = (*file->parse)(ctx, file, contents);
+    if (variables != NULL) {
+        variables_t *old = file->state.variables;
+
+        AZ(pthread_rwlock_wrlock(&file->state.rwlock));
+        file->state.variables = variables;
+        AZ(pthread_rwlock_unlock(&file->state.rwlock));
+
+        flush_variables(old);
+        free((void *) old);
+
+        result = 1;
+    }
+
+    return result;
+}
+
+static unsigned
+file_check(VRT_CTX, struct vmod_cfg_file *file, unsigned force)
+{
+    return check_remote(ctx, file->remote, force, &file_check_callback, file);
+}
+
+#define SET_STRING(value, field) \
+    do { \
+        instance->field = strdup(value); \
+        AN(instance->field); \
+    } while (0)
+
+VCL_VOID
+vmod_file__init(
+    VRT_CTX, struct vmod_cfg_file **file, const char *vcl_name,
+    VCL_STRING location, VCL_INT period,
+    VCL_INT curl_connection_timeout, VCL_INT curl_transfer_timeout,
+    VCL_BOOL curl_ssl_verify_peer, VCL_BOOL curl_ssl_verify_host,
+    VCL_STRING curl_ssl_cafile, VCL_STRING curl_ssl_capath,
+    VCL_STRING curl_proxy, VCL_ENUM format,
+    VCL_STRING name_delimiter, VCL_STRING value_delimiter)
+{
+    CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
+    AN(file);
+    AZ(*file);
+
+    struct vmod_cfg_file *instance = NULL;
+
+    if ((location != NULL) && (strlen(location) > 0) &&
+        (period >= 0) &&
+        (curl_connection_timeout >= 0) &&
+        (curl_transfer_timeout >= 0) &&
+        (name_delimiter != NULL) &&
+        (value_delimiter != NULL)) {
+        ALLOC_OBJ(instance, VMOD_CFG_FILE_MAGIC);
+        AN(instance);
+
+        instance->remote = new_remote(
+            location, period, curl_connection_timeout, curl_transfer_timeout,
+            curl_ssl_verify_peer, curl_ssl_verify_host, curl_ssl_cafile,
+            curl_ssl_capath, curl_proxy);
+        SET_STRING(name_delimiter, name_delimiter);
+        SET_STRING(value_delimiter, value_delimiter);
+        if (strcmp(format, "ini") == 0) {
+            instance->parse = &file_parse_ini;
+        } else if (strcmp(format, "json") == 0) {
+            instance->parse = &file_parse_json;
+        } else {
+            WRONG("Illegal format value.");
+        }
+        AZ(pthread_rwlock_init(&instance->state.rwlock, NULL));
+        instance->state.variables = malloc(sizeof(variables_t));
+        AN(instance->state.variables);
+        VRB_INIT(instance->state.variables);
+
+        file_check(ctx, instance, 1);
+    }
+
+    *file = instance;
+}
+
+#undef SET_STRING
+
+#define FREE_STRING(field) \
+    do { \
+        free((void *) instance->field); \
+        instance->field = NULL; \
+    } while (0)
+
+VCL_VOID
+vmod_file__fini(struct vmod_cfg_file **file)
+{
+    AN(file);
+    AN(*file);
+
+    struct vmod_cfg_file *instance = *file;
+    CHECK_OBJ_NOTNULL(instance, VMOD_CFG_FILE_MAGIC);
+
+    free_remote(instance->remote);
+    instance->remote = NULL;
+    FREE_STRING(name_delimiter);
+    FREE_STRING(value_delimiter);
+    instance->parse = NULL;
+    AZ(pthread_rwlock_destroy(&instance->state.rwlock));
+    flush_variables(instance->state.variables);
+    free((void *) instance->state.variables);
+    instance->state.variables = NULL;
+
+    FREE_OBJ(instance);
+
+    *file = NULL;
+}
+
+#undef FREE_STRING
+
+VCL_BOOL
+vmod_file_is_set(VRT_CTX, struct vmod_cfg_file *file, VCL_STRING name)
+{
+    file_check(ctx, file, 0);
+    AZ(pthread_rwlock_rdlock(&file->state.rwlock));
+    unsigned result = is_set_variable(ctx, file->state.variables, name);
+    AZ(pthread_rwlock_unlock(&file->state.rwlock));
+    return result;
+}
+
+VCL_STRING
+vmod_file_get(VRT_CTX, struct vmod_cfg_file *file, VCL_STRING name, VCL_STRING fallback)
+{
+    file_check(ctx, file, 0);
+    AZ(pthread_rwlock_rdlock(&file->state.rwlock));
+    const char *result = get_variable(ctx, file->state.variables, name, fallback);
+    AZ(pthread_rwlock_unlock(&file->state.rwlock));
+    return result;
+}
+
+VCL_STRING
+vmod_file_dump(VRT_CTX, struct vmod_cfg_file *file)
+{
+    file_check(ctx, file, 0);
+    AZ(pthread_rwlock_rdlock(&file->state.rwlock));
+    const char *result = dump_variables(ctx, file->state.variables);
+    AZ(pthread_rwlock_unlock(&file->state.rwlock));
+    return result;
+}
+
+VCL_BOOL
+vmod_file_reload(VRT_CTX, struct vmod_cfg_file *file)
+{
+    return file_check(ctx, file, 1);
+}
