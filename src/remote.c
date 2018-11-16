ec26e11b2027c4f0e85a58fc1f9ff2097679971c
Carlos Abalde <carlos.abalde@gmail.com>
2018-11-16 15:45:18 +0100

    Added 'backup' feature by @MoisesRG

--- src/remote.c
+++ src/remote.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <curl/curl.h>
+#include <sys/stat.h>
 
 #include "cache/cache.h"
 
 #include "helpers.h"
 #include "remote.h"
 
+static char *read_backup(VRT_CTX, remote_t *remote);
 static char *read_path(VRT_CTX, remote_t *remote);
 static char *read_url(VRT_CTX, remote_t *remote);
 
@@ -38,7 +40,7 @@ static char *read_url(VRT_CTX, remote_t *remote);
 
 remote_t *
 new_remote(
-    const char *location, unsigned period, unsigned curl_connection_timeout,
+    const char *location, const char *backup, unsigned period, unsigned curl_connection_timeout,
     unsigned curl_transfer_timeout, unsigned curl_ssl_verify_peer,
     unsigned curl_ssl_verify_host, const char *curl_ssl_cafile,
     const char *curl_ssl_capath, const char *curl_proxy)
@@ -56,6 +58,11 @@ new_remote(
         SET_LOCATION(url, 8);
     } else {
         SET_LOCATION(path, 0);
+    }
+    if ((backup != NULL) && (strlen(backup) > 0)) {
+        SET_STRING(backup, backup);
+    } else {
+        result->backup = NULL;
     }
     result->period = period;
     result->curl.connection_timeout = curl_connection_timeout;
@@ -94,6 +101,7 @@ free_remote(remote_t *remote)
 {
     FREE_STRING(location.raw);
     FREE_STRING(location.parsed);
+    remote->backup = NULL;
     remote->period = 0;
     remote->curl.connection_timeout = 0;
     remote->curl.transfer_timeout = 0;
@@ -117,10 +125,34 @@ free_remote(remote_t *remote)
  * HELPERS.
  *****************************************************************************/
 
+static unsigned
+check_remote_backup(
+    VRT_CTX, remote_t *remote,
+    unsigned (*callback)(VRT_CTX, void *, char *, unsigned), void *ptr)
+{
+    unsigned result = 0;
+    char *contents = read_backup(ctx, remote);
+
+    if (contents != NULL) {
+        if ((result = (*callback)(ctx, ptr, contents, 1))) {
+            LOG(ctx, LOG_INFO,
+                "Settings loaded from backup (location=%s, backup=%s)",
+                remote->location.raw, remote->backup);
+        } else {
+            LOG(ctx, LOG_ERR,
+                "Failed to load backup file (location=%s, backup=%s)",
+                remote->location.raw, remote->backup);
+        }
+        free((void *) contents);
+    }
+
+    return result;
+}
+
 unsigned
 check_remote(
     VRT_CTX, remote_t *remote, unsigned force,
-    unsigned (*callback)(VRT_CTX, void *, char *), void *ptr)
+    unsigned (*callback)(VRT_CTX, void *, char *, unsigned), void *ptr)
 {
     unsigned result = 0;
 
@@ -140,9 +172,41 @@ check_remote(
 
     if (force || winner) {
         char *contents = (*remote->read)(ctx, remote);
+        struct stat st;
         if (contents != NULL) {
-            result = (*callback)(ctx, ptr, contents);
+            result = (*callback)(ctx, ptr, contents, 0);
+            if (result && (remote->backup != NULL) && (strlen(contents) > 0)) {
+                FILE *backup = fopen(remote->backup, "wb");
+                int rc = fputs(contents, backup);
+                if (rc < 0) {
+                    char buffer[256];
+                    LOG(ctx, LOG_ERR,
+                        "Failed to write backup file (location=%s, backup=%s, error=%s)",
+                        remote->location.raw, remote->backup, strerror_r(rc, buffer, sizeof(buffer)));
+                } else {
+                    LOG(ctx, LOG_INFO,
+                        "Successfully write to backup file (location=%s, backup=%s)",
+                        remote->location.raw, remote->backup);
+                }
+                fclose(backup);
+            } else if (remote->backup != NULL) {
+                if ((stat(remote->backup, &st) == 0) && (st.st_size > 0)) {
+                    result = check_remote_backup(ctx, remote, callback, ptr);
+                } else {
+                    LOG(ctx, LOG_ERR,
+                        "Backup file is empty or doesn't exist (location=%s, backup=%s)",
+                        remote->location.raw, remote->backup);
+                }
+            }
             free((void *) contents);
+        } else if (remote->backup != NULL) {
+            if ((stat(remote->backup, &st) == 0) && (st.st_size > 0)) {
+                result = check_remote_backup(ctx, remote, callback, ptr);
+            } else {
+                LOG(ctx, LOG_ERR,
+                    "Backup file is empty or doesn't exist (location=%s, backup=%s)",
+                    remote->location.raw, remote->backup);
+            }
         }
 
         if (result || winner) {
@@ -163,20 +227,20 @@ check_remote(
 }
 
 static char *
-read_path(VRT_CTX, remote_t *remote)
+read_file(VRT_CTX, remote_t *remote, const char *file)
 {
     char *result = NULL;
 
-    FILE *fp = fopen(remote->location.parsed, "rb");
-    if (fp != NULL) {
-        fseek(fp, 0, SEEK_END);
-        unsigned long fsize = ftell(fp);
-        fseek(fp, 0, SEEK_SET);
+    FILE *bf = fopen(file, "rb");
+    if (bf != NULL) {
+        fseek(bf, 0, SEEK_END);
+        unsigned long fsize = ftell(bf);
+        fseek(bf, 0, SEEK_SET);
 
         result = malloc(fsize + 1);
         AN(result);
-        size_t nitems = fread(result, 1, fsize, fp);
-        fclose(fp);
+        size_t nitems = fread(result, 1, fsize, bf);
+        fclose(bf);
         result[fsize] = '\0';
 
         if (nitems != fsize) {
@@ -184,16 +248,29 @@ read_path(VRT_CTX, remote_t *remote)
             result = NULL;
 
             LOG(ctx, LOG_ERR,
-                "Failed to read remote (location=%s)",
-                remote->location.raw);
+                "Failed to read file (location=%s, file=%s)",
+                remote->location.raw, file);
         }
     } else {
         LOG(ctx, LOG_ERR,
-            "Failed to open remote (location=%s)",
-            remote->location.raw);
+            "Failed to open file (location=%s, file=%s)",
+            remote->location.raw, file);
     }
 
     return result;
+}
+
+static char *
+read_backup(VRT_CTX, remote_t *remote)
+{
+    AN(remote->backup);
+    return read_file(ctx, remote, remote->backup);
+}
+
+static char *
+read_path(VRT_CTX, remote_t *remote)
+{
+    return read_file(ctx, remote, remote->location.parsed);
 }
 
 struct read_url_ctx {
