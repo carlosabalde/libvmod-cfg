206343933a8455a00c93017451ade85dbce91b29
Carlos Abalde <carlos.abalde@gmail.com>
2019-05-14 13:09:07 +0200

    Better error handling in varnish.regmatch(), varnish.regsub() & varnish.regsuball()

--- src/vmod_cfg_script.c
+++ src/vmod_cfg_script.c
@@ -344,6 +344,9 @@ lua_varnish_regmatch_command(lua_State *L)
             if (!cache) {
                 VRT_re_fini(re);
             }
+        } else {
+            lua_pushfstring(L, "Failed to instantiate '%s' regexp.", regexp);
+            lua_error(L);
         }
     }
 
@@ -389,15 +392,14 @@ lua_varnish_regsub_command(lua_State *L, unsigned all)
             if (!cache) {
                 VRT_re_fini(re);
             }
+        } else {
+            lua_pushfstring(L, "Failed to instantiate '%s' regexp.", regexp);
+            lua_error(L);
         }
     }
 
     // Done!
-    if (result !=  NULL) {
-        lua_pushstring(L, result);
-    } else {
-        lua_pushnil(L);
-    }
+    lua_pushstring(L, result);
     return 1;
 }
 
