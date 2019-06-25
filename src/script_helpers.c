82b6a451f69ec28a55c2d3d5e648256d793d5c0c
Carlos Abalde <carlos.abalde@gmail.com>
2019-06-25 21:44:33 +0200

    Add initial support for ECMAScript scripts

---
+++ src/script_helpers.c
@@ -0,0 +1,501 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cache/cache.h"
+#include "vsb.h"
+#include "vsha256.h"
+#include "vre.h"
+
+#include "vtree.h"
+
+#include "script_helpers.h"
+#include "helpers.h"
+
+/******************************************************************************
+ * BASICS.
+ *****************************************************************************/
+
+static unsigned
+is_valid_engine(VRT_CTX, struct vmod_cfg_script *script, engine_t *engine)
+{
+    Lck_AssertHeld(&script->state.mutex);
+
+    if ((script->max_cycles > 0) &&
+        (engine->ncycles >= script->max_cycles)) {
+        script->state.stats.engines.dropped.cycles++;
+        return 0;
+    }
+
+    return 1;
+}
+
+engine_t *
+lock_engine(VRT_CTX, struct vmod_cfg_script *script)
+{
+    engine_t *result = NULL;
+
+    Lck_Lock(&script->state.mutex);
+
+retry:
+    while (!VTAILQ_EMPTY(&script->state.engines.free)) {
+        // Extract engine.
+        result = VTAILQ_FIRST(&script->state.engines.free);
+        CHECK_OBJ_NOTNULL(result, ENGINE_MAGIC);
+
+        // Mark the engine as busy.
+        VTAILQ_REMOVE(&script->state.engines.free, result, list);
+        VTAILQ_INSERT_TAIL(&script->state.engines.busy, result, list);
+
+        // Is the engine valid?
+        if (!is_valid_engine(ctx, script, result)) {
+            VTAILQ_REMOVE(&script->state.engines.busy, result, list);
+            script->state.engines.n--;
+            free_engine(result);
+            result = NULL;
+        } else {
+            break;
+        }
+    }
+
+    // If required, create new engine. If maximum number of engines has been
+    // reached, wait for another thread releasing an engine.
+    if (result == NULL) {
+        if (script->state.engines.n >= script->max_engines) {
+            Lck_CondWait(&script->state.engines.cond, &script->state.mutex, 0);
+            script->state.stats.workers.blocked++;
+            goto retry;
+        } else {
+            result = script->api.new_engine(ctx, script);
+            script->state.stats.engines.total++;
+            VTAILQ_INSERT_TAIL(&script->state.engines.busy, result, list);
+            script->state.engines.n++;
+        }
+    }
+
+    Lck_Unlock(&script->state.mutex);
+
+    AN(result);
+
+    return result;
+}
+
+void
+unlock_engine(VRT_CTX, struct vmod_cfg_script *script, engine_t *engine)
+{
+    CHECK_OBJ_NOTNULL(engine, ENGINE_MAGIC);
+
+    engine->memory = script->api.get_used_engine_memory(engine);
+
+    Lck_Lock(&script->state.mutex);
+    VTAILQ_REMOVE(&script->state.engines.busy, engine, list);
+    VTAILQ_INSERT_TAIL(&script->state.engines.free, engine, list);
+    AZ(pthread_cond_signal(&script->state.engines.cond));
+    Lck_Unlock(&script->state.mutex);
+}
+
+const char *
+new_function_name(const char *code)
+{
+    unsigned char sha256[SHA256_LEN];
+
+    struct SHA256Context sha_ctx;
+    SHA256_Init(&sha_ctx);
+    SHA256_Update(&sha_ctx, code, strlen(code));
+    SHA256_Final(sha256, &sha_ctx);
+
+    char *result = malloc(2 + SHA256_LEN * 2 + 1);
+    AN(result);
+    strcpy(result, "f_");
+    char *ptr = result + 2;
+    for (int i = 0; i < SHA256_LEN; i++) {
+        sprintf(ptr, "%.2x", sha256[i]);
+        ptr += 2;
+    }
+
+    return result;
+}
+
+/******************************************************************************
+ * TASK STATE.
+ *****************************************************************************/
+
+task_state_t *
+new_task_state()
+{
+    task_state_t *result;
+    ALLOC_OBJ(result, TASK_STATE_MAGIC);
+    AN(result);
+
+    reset_task_state(result);
+
+    return result;
+}
+
+void
+reset_task_state(task_state_t *state)
+{
+    state->execution.code = NULL;
+    state->execution.argc = -1;
+    memset(&state->execution.argv[0], 0, sizeof(state->execution.argv));
+    memset(&state->execution.result, 0, sizeof(state->execution.result));
+    state->execution.result.nvalues = -1;
+}
+
+void
+free_task_state(task_state_t *state)
+{
+    reset_task_state(state);
+
+    FREE_OBJ(state);
+}
+
+task_state_t *
+get_task_state(VRT_CTX, struct vmod_priv *task_priv, unsigned reset)
+{
+    task_state_t *result = NULL;
+
+    if (task_priv->priv == NULL) {
+        task_priv->priv = new_task_state();
+        task_priv->free = (vmod_priv_free_f *)free_task_state;
+        result = task_priv->priv;
+    } else {
+        result = task_priv->priv;
+        CHECK_OBJ(result, TASK_STATE_MAGIC);
+    }
+
+    if (reset) {
+        reset_task_state(result);
+    }
+
+    return result;
+}
+
+/******************************************************************************
+ * ENGINES.
+ *****************************************************************************/
+
+engine_t *
+new_engine(enum ENGINE_TYPE type, void *ctx)
+{
+    engine_t *result;
+    ALLOC_OBJ(result, ENGINE_MAGIC);
+    AN(result);
+
+    result->type = type;
+    if (result->type == ENGINE_TYPE_LUA) {
+        result->ctx.L = (lua_State *) ctx;
+        result->memory = lua_gc(result->ctx.L, LUA_GCCOUNT, 0);
+    } else if (result->type == ENGINE_TYPE_JAVASCRIPT) {
+        result->ctx.D = (duk_context *) ctx;
+        result->memory = 0;
+    }
+    result->ncycles = 0;
+
+    return result;
+}
+
+void
+free_engine(engine_t *engine)
+{
+    if (engine->type == ENGINE_TYPE_LUA) {
+        lua_close(engine->ctx.L);
+        engine->ctx.L = NULL;
+    } else if (engine->type == ENGINE_TYPE_JAVASCRIPT) {
+        duk_destroy_heap(engine->ctx.D);
+        engine->ctx.D = NULL;
+    }
+    engine->ncycles = 0;
+    engine->memory = 0;
+
+    FREE_OBJ(engine);
+}
+
+void
+flush_engines(engines_t *engines)
+{
+    engine_t *iengine;
+    while (!VTAILQ_EMPTY(engines)) {
+        iengine = VTAILQ_FIRST(engines);
+        CHECK_OBJ_NOTNULL(iengine, ENGINE_MAGIC);
+        VTAILQ_REMOVE(engines, iengine, list);
+        free_engine(iengine);
+    }
+}
+
+/******************************************************************************
+ * REGEXPS.
+ *****************************************************************************/
+
+static int
+regexpcmp(const regexp_t *v1, const regexp_t *v2)
+{
+    return strcmp(v1->text, v2->text);
+}
+
+VRBT_GENERATE(regexps, regexp, tree, regexpcmp);
+
+regexp_t *
+new_regexp(const char *text, vre_t *vre)
+{
+    regexp_t *result = NULL;
+    ALLOC_OBJ(result, REGEXP_MAGIC);
+    AN(result);
+
+    result->text = strdup(text);
+    AN(result->text);
+    result->vre = vre;
+
+    return result;
+}
+
+void
+free_regexp(regexp_t *regexp)
+{
+    free((void *) regexp->text);
+    regexp->text = NULL;
+
+    VRT_re_fini(regexp->vre);
+    regexp->vre = NULL;
+
+    FREE_OBJ(regexp);
+}
+
+vre_t *
+init_regexp(
+    VRT_CTX, struct vmod_cfg_script *script,
+    const char *regexp, unsigned cache)
+{
+
+    // Initializations.
+    vre_t *result = NULL;
+
+    // Using the cache? Try to find existing compiled regexp.
+    if (cache) {
+        regexp_t search_regexp;
+        search_regexp.text = regexp;
+        AZ(pthread_rwlock_rdlock(&script->state.rwlock));
+        regexp_t *cached_regexp = VRBT_FIND(regexps, &script->state.regexps.list, &search_regexp);
+        if (cached_regexp != NULL) {
+            result = cached_regexp->vre;
+        }
+        AZ(pthread_rwlock_unlock(&script->state.rwlock));
+    }
+
+    // Not using the cache / Not found? Compile the regexp.
+    if (result == NULL) {
+        const char *error;
+        int erroroffset;
+        result = VRE_compile(regexp, 0, &error, &erroroffset);
+
+        // Error compiling regexp?
+        if (result == NULL) {
+            LOG(ctx, LOG_ERR,
+                "Got error while compiling regexp (script=%s, regexp=%s): %s",
+                script->name, regexp, error);
+
+        // Cache result?
+        } else if (cache) {
+            regexp_t *a_regexp = new_regexp(regexp, result);
+            AZ(pthread_rwlock_wrlock(&script->state.rwlock));
+            regexp_t *cached_regexp = VRBT_FIND(regexps, &script->state.regexps.list, a_regexp);
+            if (cached_regexp == NULL) {
+                AZ(VRBT_INSERT(regexps, &script->state.regexps.list, a_regexp));
+                script->state.regexps.n++;
+            } else {
+                free_regexp(a_regexp);
+                result = cached_regexp->vre;
+            }
+            AZ(pthread_rwlock_unlock(&script->state.rwlock));
+        }
+    }
+
+    // Done!
+    return result;
+}
+
+/******************************************************************************
+ * VARNISH COMMANDS.
+ *****************************************************************************/
+
+void
+varnish_log_command(VRT_CTX, const char *message)
+{
+    AN(message);
+
+    if (ctx->vsl != NULL) {
+        VSLb(ctx->vsl, SLT_VCL_Log, "%s", message);
+    } else {
+        VSL(SLT_VCL_Log, 0, "%s", message);
+    }
+}
+
+unsigned
+varnish_regmatch_command(
+    VRT_CTX, struct vmod_cfg_script *script, const char *string,
+    const char *regexp, unsigned cache, const char **error)
+{
+    AN(string);
+    AN(regexp);
+
+    unsigned result = 0;
+    *error = NULL;
+
+    vre_t *re = init_regexp(ctx, script, regexp, cache);
+    if (re != NULL) {
+        result = VRT_re_match(ctx, string, re);
+        if (!cache) {
+            VRT_re_fini(re);
+        }
+    } else {
+        *error = WS_Printf(ctx->ws, "Failed to instantiate '%s' regexp.", regexp);
+        if (*error == NULL) {
+            FAIL_WS(ctx, 0);
+        }
+    }
+
+    return result;
+}
+
+const char *
+varnish_regsub_command(
+    VRT_CTX, struct vmod_cfg_script *script, const char *string,
+    const char *regexp, const char *sub, unsigned cache, unsigned all,
+    const char **error)
+{
+    AN(string);
+    AN(regexp);
+    AN(sub);
+
+    const char *result = NULL;
+    *error = NULL;
+
+    vre_t *re = init_regexp(ctx, script, regexp, cache);
+    if (re != NULL) {
+        result = VRT_regsub(ctx, all, string, re, sub);
+        if (!cache) {
+            VRT_re_fini(re);
+        }
+    } else {
+        *error = WS_Printf(ctx->ws, "Failed to instantiate '%s' regexp.", regexp);
+        if (*error == NULL) {
+            FAIL_WS(ctx, NULL);
+        }
+    }
+
+    return result;
+}
+
+static enum gethdr_e
+get_http_where(const char *name)
+{
+    if (strcmp(name, "req") == 0) {
+        return HDR_REQ;
+    } else if (strcmp(name, "req-top") == 0) {
+        return HDR_REQ_TOP;
+    } else if (strcmp(name, "bereq") == 0) {
+        return HDR_BEREQ;
+    } else if (strcmp(name, "resp") == 0) {
+        return HDR_RESP;
+    } else if (strcmp(name, "beresp") == 0) {
+        return HDR_BERESP;
+    } else if (strcmp(name, "obj") == 0) {
+        return HDR_OBJ;
+    } else {
+        WRONG("Invalid header type value.");
+    }
+}
+
+static unsigned
+is_valid_http_where(VRT_CTX, enum gethdr_e where)
+{
+    if (where == HDR_REQ) {
+        return ctx->http_req != NULL;
+    } else if (where == HDR_REQ_TOP) {
+        return ctx->http_req_top != NULL;
+    } else if (where == HDR_BEREQ) {
+        return ctx->http_bereq != NULL;
+    } else if (where == HDR_RESP) {
+        return ctx->http_resp != NULL;
+    } else if (where == HDR_BERESP) {
+        return ctx->http_beresp != NULL;
+    } else if (where == HDR_OBJ) {
+        return ctx->req != NULL && ctx->req->objcore != NULL;
+    } else {
+        return 0;
+    }
+}
+
+const char *
+varnish_get_header_command(
+    VRT_CTX, const char *name, const char *where, const char **error)
+{
+    AN(name);
+    AN(where);
+
+    const char *result = NULL;
+    *error = NULL;
+
+    if (strcmp(where, "req") == 0 ||
+        strcmp(where, "req-top") == 0 ||
+        strcmp(where, "bereq") == 0 ||
+        strcmp(where, "beresp") == 0 ||
+        strcmp(where, "resp") == 0 ||
+        strcmp(where, "obj") == 0) {
+        enum gethdr_e he = get_http_where(where);
+        if (is_valid_http_where(ctx, he)) {
+            char buffer[strlen(name) + 3];
+            sprintf(buffer, "%c%s:", (char) (strlen(name) + 1), name);
+            const struct gethdr_s hs = {he, buffer};
+            result = VRT_GetHdr(ctx, &hs);
+        } else {
+            *error = WS_Printf(
+                ctx->ws,
+                "varnish.get_header() called over unavailable '%s' object.",
+                where);
+            if (*error == NULL) {
+                FAIL_WS(ctx, NULL);
+            }
+        }
+    }
+
+    return result;
+}
+
+void
+varnish_set_header_command(
+    VRT_CTX, const char *name, const char *value, const char *where,
+    const char **error)
+{
+    AN(name);
+    AN(value);
+    AN(where);
+
+    *error = NULL;
+
+    if (strcmp(where, "req") == 0 ||
+        strcmp(where, "req-top") == 0 ||
+        strcmp(where, "bereq") == 0 ||
+        strcmp(where, "beresp") == 0 ||
+        strcmp(where, "resp") == 0) {
+        enum gethdr_e he = get_http_where(where);
+        if (is_valid_http_where(ctx, he)) {
+            char buffer[strlen(name) + 3];
+            sprintf(buffer, "%c%s:", (char) (strlen(name) + 1), name);
+            const struct gethdr_s hs = {he, buffer};
+            VRT_SetHdr(
+                ctx,
+                &hs,
+                value,
+                vrt_magic_string_end);
+        } else {
+            *error = WS_Printf(
+                ctx->ws,
+                "varnish.set_header() called over unavailable '%s' object.",
+                where);
+            if (*error == NULL) {
+                FAIL_WS(ctx, );
+            }
+        }
+    }
+}
---
+++ src/script_helpers.h
@@ -0,0 +1,213 @@
+#ifndef CFG_SCRIPT_H_INCLUDED
+#define CFG_SCRIPT_H_INCLUDED
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+#include "duktape.h"
+#include "remote.h"
+
+enum ENGINE_TYPE {
+    ENGINE_TYPE_LUA,
+    ENGINE_TYPE_JAVASCRIPT,
+};
+
+// regexp_t & regexps_t.
+
+typedef struct regexp {
+    unsigned magic;
+    #define REGEXP_MAGIC 0xb581daff
+
+    const char *text;
+    vre_t *vre;
+
+    VRBT_ENTRY(regexp) tree;
+} regexp_t;
+
+typedef VRBT_HEAD(regexps, regexp) regexps_t;
+
+VRBT_PROTOTYPE(regexps, regexp, tree, regexpcmp);
+
+// engine_t & engines_t.
+
+typedef struct engine {
+    unsigned magic;
+    #define ENGINE_MAGIC 0xe4eed712
+
+    enum ENGINE_TYPE type;
+    union {
+        lua_State *L;
+        duk_context *D;
+    } ctx;
+    unsigned ncycles;
+    int memory;
+
+    VTAILQ_ENTRY(engine) list;
+} engine_t;
+
+typedef VTAILQ_HEAD(engines, engine) engines_t;
+
+// result_t.
+
+enum RESULT_VALUE_TYPE {
+    RESULT_VALUE_TYPE_ERROR,
+    RESULT_VALUE_TYPE_NIL,
+    RESULT_VALUE_TYPE_BOOLEAN,
+    RESULT_VALUE_TYPE_NUMBER,
+    RESULT_VALUE_TYPE_STRING,
+    RESULT_VALUE_TYPE_TABLE
+};
+
+typedef struct result_value {
+    enum RESULT_VALUE_TYPE type;
+    union {
+        unsigned boolean;
+        double number;
+        const char *string;
+    } value;
+} result_value_t;
+
+typedef struct result {
+    int nvalues; // == -1: single value; >= 0: table with 'nvalues' values.
+    #define MAX_RESULT_VALUES 128
+    result_value_t values[MAX_RESULT_VALUES];
+} result_t;
+
+// task_state_t.
+
+typedef struct task_state {
+    unsigned magic;
+    #define TASK_STATE_MAGIC 0x803118dd
+
+    struct {
+        // Allocated in workspace: code, argv[] & result.
+        const char *code;
+        int argc; // == -1: .init() not called; >= 0: 'argc' pushed arguments.
+        #define MAX_EXECUTION_ARGS 128
+        const char *argv[MAX_EXECUTION_ARGS];
+        result_t result;
+    } execution;
+} task_state_t;
+
+// struct vmod_cfg_script.
+
+struct vmod_cfg_script {
+    unsigned magic;
+    #define VMOD_CFG_SCRIPT_MAGIC 0xed7302aa
+
+    const char *name;
+
+    remote_t *remote;
+    enum ENGINE_TYPE type;
+    unsigned max_engines;
+    unsigned max_cycles;
+    unsigned min_gc_cycles;
+    union {
+        struct {
+            unsigned gc_step_size;
+            struct {
+                unsigned loadfile;
+                unsigned dotfile;
+            } functions;
+            struct {
+                unsigned package;
+                unsigned io;
+                unsigned os;
+            } libraries;
+        } lua;
+        struct {
+        } javascript;
+    } engine_cfg;
+
+    struct {
+        engine_t *(*new_engine)(VRT_CTX, struct vmod_cfg_script *script);
+        int (*get_used_engine_memory)(engine_t *engine);
+        unsigned (*execute)(VRT_CTX, struct vmod_cfg_script *script, const char *code, const char **name, int argc, const char *argv[], result_t *result, unsigned gc_collect, unsigned flush_jemalloc_tcache);
+    } api;
+
+    struct {
+        struct lock mutex;
+        pthread_rwlock_t rwlock;
+
+        struct {
+            const char *code;
+            const char *name;
+        } function;
+
+        struct {
+            pthread_cond_t cond;
+            unsigned n;
+            engines_t free;
+            engines_t busy;
+        } engines;
+
+        struct {
+            unsigned n;
+            regexps_t list;
+        } regexps;
+
+        struct {
+            struct {
+                // Number of created scripting engines.
+                unsigned total;
+                // Number of scripting engines dropped.
+                struct {
+                    unsigned cycles;
+                } dropped;
+            } engines;
+            struct {
+                // Number of times some worker thread was blocked waiting for
+                // a free scripting engine.
+                unsigned blocked;
+            } workers;
+            struct {
+                // Total number of executions (all of them, including failed
+                // ones and syntax checks).
+                unsigned total;
+                // Number of executions registering new functions in the
+                // scripting engine.
+                unsigned unknown;
+                // Number of failed executions.
+                unsigned failed;
+                // Number of executions triggering the garbage collector.
+                unsigned gc;
+            } executions;
+        } stats;
+    } state;
+};
+
+engine_t *lock_engine(VRT_CTX, struct vmod_cfg_script *script);
+void unlock_engine(VRT_CTX, struct vmod_cfg_script *script, engine_t *engine);
+const char *new_function_name(const char *code);
+
+task_state_t *new_task_state();
+void reset_task_state(task_state_t *state);
+void free_task_state(task_state_t *state);
+task_state_t *get_task_state(VRT_CTX, struct vmod_priv *task_priv, unsigned reset);
+
+engine_t *new_engine(enum ENGINE_TYPE type, void *ctx);
+void free_engine(engine_t *engine);
+void flush_engines(engines_t *engines);
+
+regexp_t *new_regexp(const char *text, vre_t *vre);
+void free_regexp(regexp_t *regexp);
+vre_t * init_regexp(
+    VRT_CTX, struct vmod_cfg_script *script,
+    const char *regexp, unsigned cache);
+
+void varnish_log_command(VRT_CTX, const char *message);
+unsigned varnish_regmatch_command(
+    VRT_CTX, struct vmod_cfg_script *script, const char *string,
+    const char *regexp, unsigned cache, const char **error);
+const char *varnish_regsub_command(
+    VRT_CTX, struct vmod_cfg_script *script, const char *string,
+    const char *regexp, const char *sub, unsigned cache, unsigned all,
+    const char **error);
+const char *varnish_get_header_command(
+    VRT_CTX, const char *name, const char *where, const char **error);
+void varnish_set_header_command(
+    VRT_CTX, const char *name, const char *value, const char *where,
+    const char **error);
+
+#endif
